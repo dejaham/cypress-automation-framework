b6f51ddb930683f0569d625eb3d9f9deb2c30411
malik <mj.dejaham@gmail.com>
2023-04-17 18:05:57 +0200

    Commit 2

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,9 +1,24 @@
+/// <reference types= "cypress" />
+
+
 describe("Test Contact Us form via WebdriverUni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
-        // cypress code
+        
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        //cy.get('#contact-us').click({force: true})
+        cy.get('[name="first_name"]').type("Joe");
+        cy.get('[name="last_name"]').type("banner");
+        cy.get('[name="email"]').type("joeb@gmail.com");
+        cy.get('textarea.feedback-input').type("Hello i want to contact you");
+        cy.get('[type="submit"]').click();
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        // cypress code
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        //cy.get('#contact-us').click({force: true})
+        cy.get('[name="first_name"]').type("Joe");
+        cy.get('[name="last_name"]').type("banner");
+        cy.get('textarea.feedback-input').type("Hello i want to contact you");
+        cy.get('[type="submit"]').click();
     })
 })
