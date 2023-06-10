da467148a5decfada3c33486dc27dbde36e7565b
JojoSatoru <johann.dejaham@gmail.com>
2023-06-10 19:54:21 +0200

    selectors examples

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,9 +1,14 @@
-describe("Test Contact Us form via WebdriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
-        // cypress code
-    })
+/// <reference types="cypress" />
 
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        // cypress code
-    })
+
+describe("Test Contact Us form via Automation Test Store", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit("https://www.automationteststore.com/");
+        cy.get("a[href$='contact']").click();
+        //cy.xpath("//a[contains(@href,'contact')]").click();
+        cy.get('#ContactUsFrm_first_name').type("Joe");
+        cy.get('#ContactUsFrm_email').type("joe_blogs123@gmail.com");
+        cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
+        cy.get("button[title='Submit']").click();
+    });
 })
