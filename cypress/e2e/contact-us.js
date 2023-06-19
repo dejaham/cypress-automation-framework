e4f6bd57583521b529662a4745710f36b4c50041
JojoSatoru <johann.dejaham@gmail.com>
2023-06-19 19:10:14 +0200

    Merge branch 'master' of https://github.com/mdejaham/cypress-automation-framework

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /// <reference types="cypress" />
 
 
@@ -12,29 +11,4 @@ describe("Test Contact Us form via Automation Test Store", () => {
         cy.get('#ContactUsFrm_enquiry').type("Do you provide additional discount on bulk orders?")
         cy.get("button[title='Submit']").click();
     });
-=======
-/// <reference types= "cypress" />
-
-
-describe("Test Contact Us form via WebdriverUni", () => {
-    it("Should be able to submit a successful submission via contact us form", () => {
-        
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
-        //cy.get('#contact-us').click({force: true})
-        cy.get('[name="first_name"]').type("Joe");
-        cy.get('[name="last_name"]').type("banner");
-        cy.get('[name="email"]').type("joeb@gmail.com");
-        cy.get('textarea.feedback-input').type("Hello i want to contact you");
-        cy.get('[type="submit"]').click();
-    })
-
-    it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
-        //cy.get('#contact-us').click({force: true})
-        cy.get('[name="first_name"]').type("Joe");
-        cy.get('[name="last_name"]').type("banner");
-        cy.get('textarea.feedback-input').type("Hello i want to contact you");
-        cy.get('[type="submit"]').click();
-    })
->>>>>>> b6f51ddb930683f0569d625eb3d9f9deb2c30411
 })
